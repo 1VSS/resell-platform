1a56f5c0ccca790511e87c79f3a7d8071d7e722e
1vss <vstringari20@gmail.com>
2024-12-31 18:55:26 -0300

    initial commit

---
+++ src/main/java/br/com/vss/resell_platform/ResellPlatformApplication.java
@@ -0,0 +1,13 @@
+package br.com.vss.resell_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResellPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResellPlatformApplication.class, args);
+	}
+
+}
